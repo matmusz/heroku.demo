0711dee4e8641faa514015ca45324e52a82f0ac6
Mateusz <matemusz@gmail.com>
2019-04-04 21:52:52 +0200

    and another one

--- src/main/java/com/example/heroku/heroku/demo/PeopleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.heroku.heroku.demo;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface PeopleRepository extends CrudRepository<Person, Long> {
-
-
-}
